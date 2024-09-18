d4c593b4edd3d0985405d9d41e4298aa73b4b02b
smitu vernekar <81851153+smitu1@users.noreply.github.com>
2024-09-18 11:06:26 +0530

    Add files via upload

---
+++ Restaurant/index.js
@@ -0,0 +1,33 @@
+const header = document.querySelector("header");
+ 
+
+Window.addEventListener("scroll", Function(){
+    header.classList.toggle("sticky", window.scrollY > 80);
+});
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('#navlist');
+
+menu.onClick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+window.onscroll = ()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+const sr = ScrollReveal({
+    origin:'top',
+    distance :'85px',
+    duration:'2500',
+    reset: true
+})
+
+sr.reveal('.home-text',{delay:300});
+sr.reveal('.home-img',{delay:400});
+sr.reveal('.container',{delay:400});
+
+sr.reveal ('.about-img',{});
+sr.reveal('.about-text',{delay:300});
+
+sr.reveal ('.middle-text',{});
+sr.reveal('.row-btn',  '.shop-content',{delay:300});
